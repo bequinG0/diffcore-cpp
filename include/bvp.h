a6ad20682b81a6c0fce2524bcc17d0b40d7f9e00
bequ1nG0 <kulikovskikh@bk.ru>
2026-06-30 15:05:07 +0300

    modify bvp for course work after first check

--- include/bvp.h
+++ include/bvp.h
@@ -53,18 +53,35 @@ public:
 
     auto solve() -> std::pair<std::vector<double>, std::vector<double>>
     {
-        Eigen::SparseLU<Eigen::SparseMatrix<double>> solver;
-        solver.compute(A);
-        Eigen::VectorXd u = solver.solve(b);
-
-        std::vector<double> x(static_cast<size_t>(A.rows()));
-        std::vector<double> y(static_cast<size_t>(A.rows()));
-        for (int i = 0; i < A.rows(); ++i)
-        {
-            x[static_cast<size_t>(i)] = left + i * step;
-            y[static_cast<size_t>(i)] = u(i);
+        int n = A.rows();
+        std::vector<double> a(n), b_diag(n), c(n), d(n);
+        for (int i = 0; i < n; ++i) {
+            b_diag[i] = A.coeff(i, i);
+            d[i] = b(i);
+            if (i > 0)   a[i] = A.coeff(i, i-1);
+            if (i < n-1) c[i] = A.coeff(i, i+1);
         }
-        return {x, y};
+
+        std::vector<double> P(n), Q(n);
+        P[0] = -c[0] / b_diag[0];
+        Q[0] = d[0] / b_diag[0];
+
+        for (int i = 1; i < n; ++i) {
+            double denom = b_diag[i] + a[i] * P[i-1];
+            P[i] = (i < n-1) ? -c[i] / denom : 0.0;
+            Q[i] = (d[i] - a[i] * Q[i-1]) / denom;
+        }
+
+        std::vector<double> u(n);
+        u[n-1] = Q[n-1];
+        for (int i = n-2; i >= 0; --i)
+            u[i] = P[i] * u[i+1] + Q[i];
+
+        std::vector<double> x(n);
+        for (int i = 0; i < n; ++i)
+            x[i] = left + i * step;
+
+        return {x, u};
     }
 
     auto eigenSolve() -> std::pair<std::pair<double, double>, std::pair<std::vector<double>, std::vector<double>>>
@@ -87,6 +104,22 @@ public:
         }
 
         return {{lambda1, lambda2}, {u1, u2}};
+    }
+
+    auto conditionNumber() -> double
+    {
+        int N = A.rows() - 1;
+        Eigen::SparseMatrix<double> A_inner = A.block(1, 1, N-1, N-1);
+        Eigen::MatrixXd A_dense(A_inner);
+        
+        Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> solver(A_dense);
+        if (solver.info() != Eigen::Success)
+            throw std::runtime_error("EigenSolver failed");
+
+        auto lambdas = solver.eigenvalues();
+        double lambda_min = lambdas(0);
+        double lambda_max = lambdas(lambdas.size() - 1);
+        return lambda_max / lambda_min;
     }
 };
 
